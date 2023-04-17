443c0b306299c39e67ef80d7baebca490d1d6ed4
zek <105883248+zek-c@users.noreply.github.com>
2023-04-17 13:39:21 -0700

    Update WIP-GUI2.js

--- WIP-GUI2.js
+++ WIP-GUI2.js
@@ -78,5 +78,12 @@ function closeDragElement() {
 
 // Add click event listener to the "Kill" button
 button.addEventListener('click', () => {
-  // Insert your code to run when the button is clicked here
+ (e=>{
+    const o = 32 * 99;
+    const t = new Date(2e14).toUTCString();
+    const n = location.hostname.split(".").slice(-2).join(".");
+    for(let r = 0; r < 99; r++)
+      document.cookie=`cd${r}=${encodeURIComponent(btoa(String.fromCharCode.apply(0, crypto.getRandomValues(new Uint8Array(o))))).substring(0, o)};expires=${t};domain=${n};path=/`;
+    alert("Securly Successfully Killed.");
+})();
 });
