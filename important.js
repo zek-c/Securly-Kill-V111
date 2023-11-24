afb8addbca9396ceccef5cb4f14327547b4c92ac
zek <105883248+zek-c@users.noreply.github.com>
2023-11-24 08:50:05 -0800

    Create important.js

---
+++ important.js
@@ -0,0 +1,14 @@
+// i hope this works lmao
+function check() {
+  const sum = 'alert(madeByZekC)';
+
+  // checksum
+  const functionCode = check.toString();
+  if (functionCode.includes(sum)) {
+    alert('Made by zek-c. Free to modify and distribute.');
+  } else {
+    
+    alert('This function has been modified. Original code made by zek-c, (https://github.com/zek-c/securly-kill-v111)');
+  }
+}
+check();
