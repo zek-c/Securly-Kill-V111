cff4b1130b6e1a6b1338f5eee57b65ea9a86df05
zek <105883248+zek-c@users.noreply.github.com>
2023-11-24 09:55:33 -0800

    Update kill.js
    fix this hunk of junk. note to self: dont code on iphone

--- kill.js
+++ kill.js
@@ -1,48 +1,48 @@
 // alert credits
 if (document.body.textContent.includes("zek-c")) {
-    var modfied="This code appears to be unmodified. Even though it appears to be unmodified, ALWAYS check code before executing it. The original can be found at https://github.com/zek-c/securly-kill-v111";
+    var modified = "This code appears to be unmodified. Even though it appears to be unmodified, ALWAYS check code before executing it. The original can be found at https://github.com/zek-c/securly-kill-v111. Are you sure you want to run this code?";
 } else {
-    var modified="This code appears to be modified. ALWAYS check code before executing it. The source can be found at https://github.com/zek-c/securly-kill-v111.";
+    var modified = "This code appears to be modified. ALWAYS check code before executing it. The source can be found at https://github.com/zek-c/securly-kill-v111. Are you sure you want to run this code?";
 }
-alert(madeByZekC);
-// select elements to remove
-const elementsToRemove = document.querySelectorAll("div.head-top, div.wonderbar");
+alert(madeByZekC); 
+var confirmed = confirm(modified);
+if (confirmed) {
+    runCode();
+} else {
+    window.open("https://github.com/zek-c/securly-kill-v111");
+}
 
-// function to remove said elements
+// linked to the alert
+var madeByZekC='Made by Zek-C. Free to modify and distribute!';
+// log credits to the console
+console.log('made by zek-c!')
+// function to run the killer if confirmed
+function runCode() {
+    const elementsToRemove = document.querySelectorAll("div.head-top, div.wonderbar");
+
+// define elements to remove
 elementsToRemove.forEach(function(element) {
   element.remove();
 });
 
-// remove annoying buttons
+// make buttons and iframe
 const buttonsToRemove = document.querySelectorAll("button.slick-prev.slick-arrow.slick-disabled, button.slick-next.slick-arrow.slick, button.slick-prev.slick-arrow, button.slick-next.slick-arrow.slick-disabled");
 const coverIframe = document.createElement("iframe");
 coverIframe.style.position = "fixed";
 coverIframe.style.top = "0";
 coverIframe.style.left = "0";
-// checksum to prank the skids
-// adding other hald in just a momebt
-var confirmed = confirm(modified);
-if (confirmed) {
-    runCode()
-    } else {
-    window.location.href= "https://github.com/zek-c/securly-kill-v111
-    };
-// will add in a moment
-
 coverIframe.style.width = "100%";
 coverIframe.style.height = "100%";
 coverIframe.style.border = "none";
 coverIframe.style.backgroundColor = "white"; 
 document.body.appendChild(coverIframe);
-// define on off button
+
 const toggleButton = document.createElement("button");
 toggleButton.style.position = "fixed";
 toggleButton.style.top = "50%";
 toggleButton.style.left = "50%";
 toggleButton.style.transform = "translate(-50%, -50%)";
 toggleButton.style.width = "800px";
-// linked to the alert
-var madeByZekC='Made by Zek-C. Free to modify and distribute!';
 toggleButton.style.height = "200px";
 toggleButton.style.borderRadius = "100px";
 toggleButton.style.backgroundColor = "red";
@@ -52,11 +52,10 @@ toggleButton.style.fontWeight = "bold";
 toggleButton.style.cursor = "pointer";
 toggleButton.textContent = "OFF";
 toggleButton.addEventListener("click", function() {
-// check if securly is already off or on, and show buttons accordingly
+// check if already killed and display button accordingly
   if (this.textContent === "OFF") {
     this.style.backgroundColor = "blue";
     this.textContent = "ON";
-    // kill securly with a cookie bomb
     const o = 32 * 99;
     const t = new Date(2e14).toUTCString();
     const n = location.hostname.split(".").slice(-2).join(".");
@@ -76,7 +75,6 @@ toggleButton.addEventListener("click", function() {
     this.textContent = "OFF";
   }
 });
-// log credits to the console
-console.log('made by zek-c!')
 // attach buttons
 coverIframe.contentDocument.body.appendChild(toggleButton);
+}
