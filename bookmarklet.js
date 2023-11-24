28030b2c95b10f2b95145b5cd05f580e23df8199
zek <105883248+zek-c@users.noreply.github.com>
2023-11-24 09:56:11 -0800

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:fetch("https://raw.githubusercontent.com/zek-c/Securly-Kill-V111/main/kill.js").then(r => r.text()).then(r => eval(r))
+javascript:fetch("https://raw.githubusercontent.com/zek-c/Securly-Kill-V111/main/kill.js").then(r => r.text()).then(r => eval(r)) // made by zek-c
